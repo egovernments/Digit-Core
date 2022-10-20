bb5818a227665c64fc79a32645e38c0fcd5ac600
shubhang-egov <shubhang.dhanesha@egovernments.org>
2022-10-20 11:29:40 +0530

    Removed db timezone dependency

--- core-services/egov-otp/src/main/java/org/egov/persistence/repository/TokenRepository.java
+++ core-services/egov-otp/src/main/java/org/egov/persistence/repository/TokenRepository.java
@@ -11,7 +11,7 @@ import org.egov.domain.model.TokenSearchCriteria;
 import org.egov.domain.model.Tokens;
 import org.egov.domain.model.ValidateRequest;
 import org.egov.persistence.repository.rowmapper.TokenRowMapper;
-import org.egov.web.util.OtpConfiguration;
+import org.egov.web.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -22,10 +22,10 @@ public class TokenRepository {
     private static final int UPDATED_ROWS_COUNT = 1;
     private static final String NO = "N";
     private static final String INSERT_TOKEN = "insert into eg_token(id,tenantid,tokennumber,tokenidentity,validated,ttlsecs,createddate,createdby,version,createddatenew) values (:id,:tenantId,:tokenNumber,:tokenIdentity,:validated,:ttlSecs,:createdDate,:createdBy,:version,:createddatenew);";
-    private static final String GETTOKENS_BY_NUMBER_IDENTITY_TENANT = "select * from eg_token where tokenidentity=:tokenIdentity and tenantid=:tenantId and ((extract(epoch from now()) * 1000 - createddatenew)/1000)::int <= ttlsecs and validated = 'N'";
+    private static final String GETTOKENS_BY_NUMBER_IDENTITY_TENANT = "select * from eg_token where tokenidentity=:tokenIdentity and tenantid=:tenantId and ((:timestamp - createddatenew)/1000)::int <= ttlsecs and validated = 'N'";
     private static final String UPDATE_TOKEN = "update eg_token set validated = 'Y' where id = :id";
     private static final String GETTOKEN_BYID = "select * from eg_token where id=:id";
-    private static final String UPDATETOKEN_TLL_BYID = "update eg_token set ttlsecs = (extract (epoch from now()) - createddatenew / 1000)::int + :ttl where id = :id";
+    private static final String UPDATETOKEN_TLL_BYID = "update eg_token set ttlsecs = ((:timestamp - createddatenew) / 1000)::int + :ttl where id = :id";
 
     @Autowired
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
@@ -84,6 +84,7 @@ public class TokenRepository {
         final Map<String, Object> tokenInputs = new HashMap<String, Object>();
         tokenInputs.put("tokenIdentity", request.getIdentity());
         tokenInputs.put("tenantId", tenantId);
+        tokenInputs.put("timestamp", System.currentTimeMillis());
         List<Token> domainTokens = namedParameterJdbcTemplate.query(GETTOKENS_BY_NUMBER_IDENTITY_TENANT, tokenInputs,
                 new TokenRowMapper());
         return new Tokens(domainTokens);
@@ -106,6 +107,7 @@ public class TokenRepository {
         final Map<String, Object> tokenInputs = new HashMap<String, Object>();
         tokenInputs.put("id", t.getUuid());
         tokenInputs.put("ttl", otpConfiguration.getTtl());
+        tokenInputs.put("timestamp", System.currentTimeMillis());
         return namedParameterJdbcTemplate.update(UPDATETOKEN_TLL_BYID, tokenInputs);
     }
 }
