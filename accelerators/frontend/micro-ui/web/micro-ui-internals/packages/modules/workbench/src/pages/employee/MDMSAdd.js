74e99f79eef1eab5350b3cb697ab2b190a9e5a1e
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-07-18 11:28:06 +0530

    formatted data

--- accelerators/frontend/micro-ui/web/micro-ui-internals/packages/modules/workbench/src/pages/employee/MDMSAdd.js
+++ accelerators/frontend/micro-ui/web/micro-ui-internals/packages/modules/workbench/src/pages/employee/MDMSAdd.js
@@ -2,147 +2,9 @@ import { Loader, FormComposer } from "@egovernments/digit-ui-react-components";
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useHistory } from "react-router-dom";
-import { mdmsSchema, schema } from "../../configs/sampleschema";
+import { mdmsSchema } from "../../configs/sampleschema";
 import _ from "lodash";
 
-export const newConfig = [
-  {
-    head: "Config Header",
-    subHead: "Config Sub Head",
-    body: [
-      {
-        inline: true,
-        label: "Enter Sample text 1",
-        isMandatory: false,
-        key: "BrSelectFather",
-        type: "text",
-        disable: false,
-        populators: { name: "sampletext1", error: "Required", validation: { pattern: /^[A-Za-z]+$/i } },
-      },
-      {
-        inline: true,
-        label: "Enter Sample text 333",
-        isMandatory: true,
-        key: "BrSelectFather",
-        type: "text",
-        disable: false,
-        populators: { name: "sampletext333", error: "Required", validation: { pattern: /^[A-Za-z]+$/i } },
-      },
-      {
-        inline: true,
-        label: "Enter Sample text 2",
-        isMandatory: false,
-        description: "Field supporting description",
-        key: "BrSelectFather",
-        type: "text",
-        disable: false,
-        populators: { name: "sampletext2", error: "Required", validation: { pattern: /^[A-Za-z]+$/i } },
-      },
-      {
-        inline: true,
-        label: "Enter Sample Dob",
-        isMandatory: true,
-        description: "Field supporting description",
-        type: "date",
-        disable: false,
-        populators: { name: "dob", error: "Required", validation: { required: true } },
-      },
-      {
-        label: "Enter Sample phone number",
-        isMandatory: true,
-        key: "BrSelectFather",
-        type: "number",
-        disable: false,
-        populators: { name: "samplenum1", error: "sample error message", validation: { min: 5999999999, max: 9999999999 } },
-      },
-      {
-        label: "Enter Sample phone number 2",
-        isMandatory: true,
-        key: "BrSelectFather",
-        type: "mobileNumber",
-        disable: false,
-        populators: { name: "samplenum2", error: "sample error message", validation: { min: 5999999999, max: 9999999999 } },
-      },
-    ],
-  },
-  {
-    head: "Config Header 2",
-    body: [
-      {
-        label: "Enter text 2",
-        isMandatory: true,
-        description: "Field supporting description",
-        key: "BrSelectFather",
-        type: "text",
-        disable: false,
-        populators: { name: "sampletext3", error: "sample error message", validation: { pattern: /^[A-Za-z]+$/i } },
-      },
-      {
-        isMandatory: true,
-        key: "genders",
-        type: "radioordropdown",
-        label: "Enter Gender",
-        disable: false,
-        populators: {
-          name: "genders",
-          optionsKey: "name",
-          error: "sample required message",
-          required: true,
-          mdmsConfig: {
-            masterName: "GenderType",
-            moduleName: "common-masters",
-            localePrefix: "COMMON_GENDER",
-          },
-        },
-      },
-      {
-        isMandatory: false,
-        key: "gender",
-        type: "radio",
-        label: "Enter Gender New",
-        disable: false,
-        populators: {
-          name: "gender",
-          optionsKey: "name",
-          error: "sample required message",
-          required: false,
-          options: [
-            {
-              code: "MALE",
-              name: "MALE",
-            },
-            {
-              code: "FEMALE",
-              name: "FEMALE",
-            },
-            {
-              code: "TRANSGENDER",
-              name: "TRANSGENDER",
-            },
-          ],
-        },
-      },
-      {
-        isMandatory: true,
-        key: "gender3",
-        type: "radio",
-        label: "Enter Gender",
-        disable: false,
-        populators: {
-          name: "gender3",
-          optionsKey: "name",
-          error: "sample required message",
-          required: true,
-          mdmsConfig: {
-            masterName: "GenderType",
-            moduleName: "common-masters",
-            localePrefix: "COMMON_GENDER",
-          },
-        },
-      },
-    ],
-  },
-];
 
 const MDMSAdd = ({ FormSession }) => {
   const tenantId = Digit.ULBService.getCurrentTenantId();
@@ -155,6 +17,7 @@ const MDMSAdd = ({ FormSession }) => {
 
   const onSubmit = (data) => {
     const formattedData = Digit.Utils.workbench.getFormattedData(data);
+    console.log(formattedData,"entered data")
   };
 
   const onFormValueChange = (setValue, formData, formState) => {
@@ -184,7 +47,7 @@ const MDMSAdd = ({ FormSession }) => {
   /* use newConfig instead of commonFields for local development in case needed */
 
   const configs = Digit.Hooks.workbench.UICreateConfigGenerator(mdmsSchema, {});
-  // const configs = newConfig ? newConfig : newConfig;
+
   return (
     <FormComposer
       heading={t("Add MDMS Data")}
