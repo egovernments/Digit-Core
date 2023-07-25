8c2555d39327a5280cb267a112e29e879a2eec06
Ankita <rklankitasahoo2001@gmail.com>
2023-07-25 14:47:05 +0530

    Updated xml file and User at egov-user

--- core-services/egov-user/src/main/java/org/egov/user/web/controller/User.java
+++ core-services/egov-user/src/main/java/org/egov/user/web/controller/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private long id;
     private String username;
     private String password;
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
