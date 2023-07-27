d81c56e56f59af8e81eef17e53413f8442f6eb67
ajay-egov <95418896+ajay-egov@users.noreply.github.com>
2023-07-27 16:38:53 +0530

    Add compare two indices util to metric chart handler (#215)
    * PFM-2922 | Add metricCount to metric chart
    
    * Add field check to additive computed field
    
    * Add compareTwoindices util to metric chart handler

--- business-services/dashboard-analytics/src/main/java/com/tarento/analytics/handler/IResponseHandler.java
+++ business-services/dashboard-analytics/src/main/java/com/tarento/analytics/handler/IResponseHandler.java
@@ -75,6 +75,8 @@ public interface IResponseHandler {
     public final String DOC_COUNT = "doc_count"; 
     
     public static final String POST_AGGREGATION_THEORY = "postAggregationTheory";
+
+	public static final String COMPARE_TWO_INDICES = "compareTwoIndices";
     
     public static final String CHART_SPECIFIC = "chartSpecificProperty";
 	
@@ -205,6 +207,19 @@ public interface IResponseHandler {
 		}
 		logger.info("after appending missing plots : "+ sortedMap);
 		data.setPlots(sortedMap.values().stream().collect(Collectors.toList()));
+	}
+
+	default Double compareTwoIndices(List<List<String>> bucketList){
+		List<String> listA = bucketList.get(0);
+		List<String> listB = bucketList.get(1);
+
+		Double count = 0.0;
+		for (String str : listA) {
+			if (!listB.contains(str)) {
+				count++;
+			}
+		}
+		return  count;
 	}
 
 }
