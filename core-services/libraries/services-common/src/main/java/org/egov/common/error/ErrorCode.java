0824eb711af8ca64436fffffcf9cbad9fcaa440e
Shashwat Mishra <shashwat.mishra@egovernments.org>
2023-07-27 11:53:08 +0530

    [DPG-1427] Making changes to common library

--- core-services/libraries/services-common/src/main/java/org/egov/common/error/ErrorCode.java
+++ core-services/libraries/services-common/src/main/java/org/egov/common/error/ErrorCode.java
@@ -9,6 +9,8 @@ public class ErrorCode {
 
     public static final String USER_INFO_NULL_ERROR_MESSAGE = "UserInfo present inside RequestInfo being sent to enrichAuditDetails method must not be null";
 
+    public static final String USER_UUID_NULL_ERROR_MESSAGE = "User uuid present inside UserInfo being sent to enrichAuditDetails method must not be null";
+
     public static final String CREATED_TIME_NULL_ERROR_MESSAGE = "CreatedTime present inside AuditDetails being sent to enrichAuditDetails method must not be null in case of update request";
 
     public static final String CREATED_BY_NULL_ERROR_MESSAGE = "CreatedBy present inside AuditDetails being sent to enrichAuditDetails method must not be null in case of update request";
--- core-services/libraries/services-common/src/main/java/org/egov/common/utils/AuditDetailsEnrichmentUtil.java
+++ core-services/libraries/services-common/src/main/java/org/egov/common/utils/AuditDetailsEnrichmentUtil.java
@@ -18,9 +18,16 @@ public class AuditDetailsEnrichmentUtil {
      * @param requestInfo
      * @param isCreateRequest
      */
-    public static void enrichAuditDetails(AuditDetails auditDetails, RequestInfo requestInfo, Boolean isCreateRequest) {
+    public static AuditDetails prepareAuditDetails(AuditDetails auditDetails, RequestInfo requestInfo, Boolean isCreateRequest) {
         // This method validates if auditDetails, requestInfo and userInfo are not null.
         validateInput(auditDetails, requestInfo, isCreateRequest);
+
+        // Check if isCreateRequest flag is true - in this case, enrich createdTime and createdBy fields.
+        if(isCreateRequest){
+            auditDetails = AuditDetails.builder().build();
+            auditDetails.setCreatedTime(System.currentTimeMillis());
+            auditDetails.setCreatedBy(requestInfo.getUserInfo().getUuid());
+        }
 
         // Update lastModifiedBy and lastModifiedTime fields for both create and update requests.
         auditDetails.setLastModifiedTime(System.currentTimeMillis());
@@ -31,6 +38,8 @@ public class AuditDetailsEnrichmentUtil {
             auditDetails.setCreatedTime(System.currentTimeMillis());
             auditDetails.setCreatedBy(requestInfo.getUserInfo().getUuid());
         }
+
+        return auditDetails;
     }
 
     /**
@@ -44,10 +53,6 @@ public class AuditDetailsEnrichmentUtil {
         if(ObjectUtils.isEmpty(isCreateRequest))
             throw new NullCheckException(IS_CREATE_REQUEST_FLAG_NULL_ERROR_MESSAGE);
 
-        // Throw null check exception in case auditDetails is null.
-        if(ObjectUtils.isEmpty(auditDetails))
-            throw new NullCheckException(AUDIT_DETAILS_NULL_ERROR_MESSAGE);
-
         // Throw null check exception in case requestInfo is null.
         if(ObjectUtils.isEmpty(requestInfo))
             throw new NullCheckException(REQUEST_INFO_NULL_ERROR_MESSAGE);
@@ -56,8 +61,16 @@ public class AuditDetailsEnrichmentUtil {
         if(ObjectUtils.isEmpty(requestInfo.getUserInfo()))
             throw new NullCheckException(USER_INFO_NULL_ERROR_MESSAGE);
 
+        // Throw null check exception in case userInfo within requestInfo is null.
+        if(ObjectUtils.isEmpty(requestInfo.getUserInfo().getUuid()))
+            throw new NullCheckException(USER_UUID_NULL_ERROR_MESSAGE);
+
         // Check whether createdBy and createdTime fields are set within auditDetails in case of update request.
         if(!isCreateRequest) {
+            // Throw null check exception in case auditDetails is null.
+            if(ObjectUtils.isEmpty(auditDetails))
+                throw new NullCheckException(AUDIT_DETAILS_NULL_ERROR_MESSAGE);
+
             // Throw null check exception in case createdBy within auditDetails is null
             if (ObjectUtils.isEmpty(auditDetails.getCreatedBy()))
                 throw new NullCheckException(CREATED_BY_NULL_ERROR_MESSAGE);
