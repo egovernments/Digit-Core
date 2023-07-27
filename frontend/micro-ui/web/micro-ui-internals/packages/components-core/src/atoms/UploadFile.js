7c5d5b06d4dde7528b3cb256ed4d65e4eb8a06c2
Anil Singha <99383116+anilsingha-eGov@users.noreply.github.com>
2023-07-27 11:48:20 +0530

    DPG-1708:Added all remaining Atoms (#5687)
    * DPG-1708:Added Radio Buttons
    
    * DPG-1708:Added MultiSelectdropdown
    
    * DPG-1708:Added UploadFile
    
    * DPG-1708:Updated PR comment
    
    * DPG-1708:Updated PR comment
    
    * DPG-1708:Updated inline styles
    
    * DPG-1708:Updated inline styles
    
    * DPG-1708:Updated inline styles

---
+++ frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/atoms/UploadFile.js
@@ -0,0 +1,115 @@
+import React, { useEffect, useRef, useState, Fragment } from "react";
+import PropTypes from "prop-types";
+import Button from "./Button";
+import { useTranslation } from "react-i18next";
+import RemoveableTag from "./RemoveableTag";
+import { getUserType } from "../utils";
+import ErrorMessage from "./ErrorMessage";
+
+const getRandomId = () => {
+  return Math.floor((Math.random() || 1) * 139);
+};
+
+const UploadFile = (props) => {
+  const { t } = useTranslation();
+  const inpRef = useRef();
+  const [hasFile, setHasFile] = useState(false);
+  const [prevSate, setprevSate] = useState(null);
+  const user_type = getUserType();
+  const handleChange = () => {
+    if (inpRef.current.files[0]) {
+      setHasFile(true);
+      setprevSate(inpRef.current.files[0]);
+    } else setHasFile(false);
+  };
+
+  const handleDelete = () => {
+    inpRef.current.value = "";
+    props.onDelete();
+  };
+
+  const handleEmpty = () => {
+    if (inpRef.current.files.length <= 0 && prevSate !== null) {
+      inpRef.current.value = "";
+      props.onDelete();
+    }
+  };
+
+  if (props.uploadMessage && inpRef.current.value) {
+    handleDelete();
+    setHasFile(false);
+  }
+
+  useEffect(() => handleEmpty(), [inpRef?.current?.files]);
+
+  useEffect(() => handleChange(), [props.message]);
+
+  const showHint = props?.showHint || false;
+
+  return (
+    <Fragment>
+      {showHint && <p className="digit-cell-text">{t(props?.hintText)}</p>}
+      <div
+        className={`digit-upload-file ${props?.customClass ? props?.customClass : ""} ${
+          user_type === "employee" ? "" : "digit-upload-file-max-width"
+        } ${props.disabled ? " disabled" : ""}`}
+        style={props?.style}
+      >
+        <div className="digit-upload-file-button-wrap">
+          <Button theme="border" label={t("CS_COMMON_CHOOSE_FILE")} textStyles={props?.textStyles} type={props.buttonType} />
+          {props?.uploadedFiles?.map((file, index) => {
+            const fileDetailsData = file[1];
+            return (
+              <div className="digit-tag-container">
+                <RemoveableTag key={index} text={file[0]} onClick={(e) => props?.removeTargetedFile(fileDetailsData, e)} />
+              </div>
+            );
+          })}
+          {props?.uploadedFiles.length === 0 && <h2 className="digit-file-upload-status">{props.message}</h2>}
+        </div>
+        <input
+          className={props.disabled ? "disabled" : "" + "digit-input-mirror-selector-button"}
+          ref={inpRef}
+          type="file"
+          id={props.id || `document-${getRandomId()}`}
+          name="file"
+          multiple={props.multiple}
+          accept={props.accept}
+          disabled={props.disabled}
+          onChange={(e) => props.onUpload(e)}
+          onClick={(event) => {
+            if (!props?.enableButton) {
+              event.preventDefault();
+            } else {
+              const { target = {} } = event || {};
+              target.value = "";
+            }
+          }}
+        />
+      </div>
+      {props.iserror && <ErrorMessage message={props.iserror} />}
+      {props?.showHintBelow && <p className="digit-cell-text">{t(props?.hintText)}</p>}
+    </Fragment>
+  );
+};
+UploadFile.propTypes = {
+  hintText: PropTypes.string,
+  customClass: PropTypes.string,
+  uploadedFiles: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.object]))),
+  enableButton: PropTypes.bool,
+  showHint: PropTypes.bool,
+  buttonType: PropTypes.string,
+  onUpload: PropTypes.func.isRequired,
+  removeTargetedFile: PropTypes.func.isRequired,
+  onDelete: PropTypes.func,
+  iserror: PropTypes.string,
+  showHintBelow: PropTypes.bool,
+  message: PropTypes.string,
+  disabled: PropTypes.bool,
+  inputStyles: PropTypes.object,
+  multiple: PropTypes.bool,
+  accept: PropTypes.string,
+  id: PropTypes.string,
+};
+
+export default UploadFile;
---
+++ frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/stories/MultiSelectDropdown.stories.js
@@ -0,0 +1,54 @@
+import React from "react";
+import MultiSelectDropdown from "../atoms/MultiSelectDropdown";
+import { action } from "@storybook/addon-actions";
+
+export default {
+  title: "Atoms/MultiSelectDropdown",
+  component: MultiSelectDropdown,
+  argTypes: {
+    options: { control: "object" },
+    selected: { control: "array" },
+    defaultLabel: { control: "text" },
+    defaultUnit: { control: "text" },
+    BlockNumber: { control: "number" },
+    isOBPSMultiple: { control: "boolean" },
+    isPropsNeeded: { control: "boolean" },
+    ServerStyle: { control: "object" },
+    config: { control: "object" },
+  },
+};
+
+const options = [
+  { id: 1, name: "Option 1" },
+  { id: 2, name: "Option 2" },
+  { id: 3, name: "Option 3" },
+];
+
+const onSelectAction = action("onSelect");
+
+const Template = (args) => (
+  <MultiSelectDropdown
+    {...args}
+    onSelect={(selectedOptions) => {
+      onSelectAction(selectedOptions);
+      // Implement the onSelect handler here
+      console.log("Selected Options:", selectedOptions);
+    }}
+  />
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  options,
+  optionsKey: "name",
+  selected: [],
+  defaultLabel: "Select Options",
+};
+
+export const WithSelectedOptions = Template.bind({});
+WithSelectedOptions.args = {
+  options,
+  optionsKey: "name",
+  selected: [options[0], options[2]],
+  defaultLabel: "Select Options",
+};
