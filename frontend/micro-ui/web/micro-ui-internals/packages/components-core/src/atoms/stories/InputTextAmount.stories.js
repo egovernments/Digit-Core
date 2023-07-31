cf2f0b3c22f4e40cc3da594e34e50ac62ce66458
Anil Singha <99383116+anilsingha-eGov@users.noreply.github.com>
2023-07-31 16:58:07 +0530

    DPG-1708:Added InputextAmount,Fomatter (#5689)
    * DPG-1708:Added InputextAmount,Fomatter
    
    * DPG-1708:Added InputextAmount,Fomatter
    
    * DPG-1708:Refactored InputTextAmount code

---
+++ frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/atoms/stories/InputTextAmount.stories.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import InputTextAmount from "../InputTextAmount";
+export default {
+  title: "Components/InputTextAmount",
+  component: InputTextAmount,
+};
+
+const Template = (args) => <InputTextAmount {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  value: "",
+  prefix: "₹ ",
+  onChange: action("Amount changed"),
+};
+
+export const WithValue = Template.bind({});
+WithValue.args = {
+  value: "1000.5",
+  prefix: "₹ ",
+  onChange: action("Amount changed"),
+};
+
+export const WithDisabledState = Template.bind({});
+WithDisabledState.args = {
+  value: "500.75",
+  prefix: "₹ ",
+  onChange: action("Amount changed"),
+  disabled: true,
+};
+
+export const WithCustomStyle = Template.bind({});
+WithCustomStyle.args = {
+  value: "2500",
+  prefix: "₹ ",
+  onChange: action("Amount changed"),
+  style: { color: "green", fontSize: "18px", fontWeight: "bold" },
+};
+
+export const WithCustomDecimalScale = Template.bind({});
+WithCustomDecimalScale.args = {
+  value: "1234.5678",
+  prefix: "₹ ",
+  onChange: action("Amount changed"),
+  decimalScale: 3,
+};
+
+export const WithCustomStep = Template.bind({});
+WithCustomStep.args = {
+  value: "0",
+  prefix: "₹ ",
+  onChange: action("Amount changed"),
+  step: 100,
+};
---
+++ frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/utils/inputAmountWrapper.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from "react";
+import TextInput from "../atoms/TextInput";
+import { CurrencyInput } from "./currencyInput";
+
+const DEFAULT_PREFIX = "₹ ";
+const LIMIT = 500;
+
+export const InputAmountWrapper = ({ ref, ...props }) => {
+  const prefix = props?.prefix || DEFAULT_PREFIX;
+
+  const [errorMessage, setErrorMessage] = useState("");
+  const [className, setClassName] = useState("");
+  const [value, setValue] = useState(props?.defaultValue);
+  const [values, setValues] = useState();
+  const [rawValue, setRawValue] = useState(" ");
+
+  useEffect(() => {
+    props.onValueChange(value);
+  }, [value]);
+
+  /**
+   * Handle validation
+   */
+  const handleOnValueChange = (value, _, values) => {
+    setValues(values);
+    setRawValue(value === undefined ? "undefined" : value || " ");
+
+    if (!value) {
+      setClassName("");
+      setValue("");
+      return;
+    }
+
+    if (Number.isNaN(Number(value))) {
+      // setErrorMessage('Please enter a valid number');
+      // setClassName('is-invalid');
+      return;
+    }
+
+    if (Number(value) > LIMIT) {
+      // setErrorMessage(`Max: ${prefix}${LIMIT}`);
+      // setClassName('is-invalid');
+      setValue(value);
+      return;
+    }
+
+    // setClassName('is-valid');
+    setValue(value);
+  };
+
+  return (
+    <CurrencyInput
+      id="validationCustom01"
+      name="input-1"
+      customInput={TextInput}
+      className={`form-control ${className}`}
+      value={props.defaultValue ? props.defaultValue : value ? value : ""}
+      onValueChange={handleOnValueChange}
+      // placeholder="Please enter a number"
+      prefix={prefix}
+      step={1}
+      otherProps={props?.otherProps}
+      inputRef={ref}
+    />
+  );
+};
+
+export default InputAmountWrapper;
