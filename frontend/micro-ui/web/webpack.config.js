1127699274eeef7a68dcef2b8917573252bc58bb
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-08-02 22:05:52 +0530

    Update webpack.config.js

--- frontend/micro-ui/web/webpack.config.js
+++ frontend/micro-ui/web/webpack.config.js
@@ -9,9 +9,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
     ],
   },
