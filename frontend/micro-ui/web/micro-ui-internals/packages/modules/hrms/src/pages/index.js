78cb025ae89a46dc47a1a8607ecceef43c58cf72
nabeelmd-eGov <94039229+nabeelmd-eGov@users.noreply.github.com>
2023-08-09 16:48:31 +0530

    DPG-1714: CSS issue fix for formcomposerv2 fields (#168)
    Co-authored-by: nabeelmd-eGov <nabeel.md@egovernments.com>

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/hrms/src/pages/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/hrms/src/pages/index.js
@@ -3,7 +3,6 @@ import React,{ useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { Link, Switch, useLocation } from "react-router-dom";
 
-const {SixFtApart,Rotate360}=SVG;
 const EmployeeApp = ({ path, url, userType }) => {
   const { t } = useTranslation();
   const location = useLocation();
