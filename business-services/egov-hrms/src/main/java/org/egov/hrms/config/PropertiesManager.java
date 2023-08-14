734fcce9f779c699dd7ae29f323be373702b142b
JithendarKumar-eGov <103918846+JithendarKumar-eGov@users.noreply.github.com>
2023-08-14 12:02:25 +0530

    Removed reference to parentlevel (#176)

--- business-services/egov-hrms/src/main/java/org/egov/hrms/config/PropertiesManager.java
+++ business-services/egov-hrms/src/main/java/org/egov/hrms/config/PropertiesManager.java
@@ -111,9 +111,6 @@ public class PropertiesManager {
 	@Value("${state.level.tenant.id}")
 	public String stateLevelTenantId;
 
-	@Value("${parent.level.tenant.id}")
-	private String parentLevelTenantId;
-
 	@Value("${decryption.abac.enable}")
 	private Boolean isDecryptionEnable;
 }
