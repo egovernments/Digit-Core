853e842a37ea09664dd25c9dee73536b17e3dbaf
Priyanka-eGov <74049060+Priyanka-eGov@users.noreply.github.com>
2023-08-14 12:00:39 +0530

    Inbox central instance fix (#172)
    * Updating inbox service with code changes from Core-platform
    
    * Updating inbox service with code changes from Digit-dev

--- accelerators/inbox/src/main/java/org/egov/inbox/util/InboxConstants.java
+++ accelerators/inbox/src/main/java/org/egov/inbox/util/InboxConstants.java
@@ -29,7 +29,11 @@ public class InboxConstants {
 
     public static final String CREATED_TIME_KEY = "createdTime";
 
+    public static final String LAST_MODIFIED_TIME_KEY = "lastModifiedTime";
+
     public static final String BUSINESS_SERVICE_PATH = "$.currentProcessInstance.businessService";
+
+    public static final String STATE_UUID_PATH = "$.currentProcessInstance.state.uuid";
 
     public static final String APPLICATION_STATUS_KEY = "applicationstatus";
 
