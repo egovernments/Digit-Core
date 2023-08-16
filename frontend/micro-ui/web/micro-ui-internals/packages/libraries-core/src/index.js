b78fb150900d06f6b4c8d30979fb58dd01d0c033
Anil Singha <99383116+anilsingha-eGov@users.noreply.github.com>
2023-08-16 16:24:55 +0530

    DPG-1724: code clean up for molecules,atoms

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries-core/src/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries-core/src/index.js
@@ -3,20 +3,14 @@ import mergeConfig from "./config/mergeConfig";
 import { useStore } from "./services/index";
 import { initI18n } from "./translations/index";
 import { Storage, PersistantStorage } from "./services/atoms/Utils/Storage";
-import { UploadServices } from "./services/atoms/UploadServices";
-import JsDictionary from "./services/atoms/JsDictionary";
 import { LocationService } from "./services/elements/Location";
 import { LocalityService } from "./services/elements/Localities";
 import { LocalizationService } from "./services/elements/Localization/service";
-import { LoginService } from "./services/elements/Login";
 import * as dateUtils from "./services/atoms/Utils/Date";
-import Download from "./services/atoms/Download";
-import { WorkflowService } from "./services/elements/WorkFlow";
 import { MdmsService } from "./services/elements/MDMS";
 import { ULBService } from "./services/molecules/Ulb";
 import { ComponentRegistryService } from "./services/elements/ComponentRegistry";
 import StoreData from "./services/molecules/StoreData";
-
 import Contexts from "./contexts";
 import Hooks from "./hooks";
 import Utils from "./utils";
@@ -37,20 +31,15 @@ const initCoreLibraries = () => {
   setupLibraries("Services", { useStore });
   setupLibraries("LocationService", LocationService);
   setupLibraries("LocalityService", LocalityService);
-  setupLibraries("LoginService", LoginService);
   setupLibraries("LocalizationService", LocalizationService);
   setupLibraries("DateUtils", dateUtils);
-  setupLibraries("WorkflowService", WorkflowService);
   setupLibraries("MDMSService", MdmsService);
-  setupLibraries("UploadServices", UploadServices);
-  setupLibraries("JsDictionary", JsDictionary);
   setupLibraries("ComponentRegistryService", ComponentRegistryService);
   setupLibraries("StoreData", StoreData);
   setupLibraries("Contexts", Contexts);
   setupLibraries("Hooks", Hooks);
   setupLibraries("Customizations", {});
   setupLibraries("Utils", Utils);
-  setupLibraries("Download", Download);
 
   return new Promise((resolve) => {
     initI18n(resolve);
