ec1956e5a6525c68239c362da0f85d9f341d10ed
Anil Singha <99383116+anilsingha-eGov@users.noreply.github.com>
2023-08-16 16:07:52 +0530

    DPG-1724: Unwanted code removed

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries-core/src/hooks/index.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries-core/src/hooks/index.js
@@ -4,31 +4,25 @@ import useCustomMDMS from "./useCustomMDMS";
 import useLocation from "./useLocation";
 import useBoundaryLocalities from "./useLocalities";
 import useCommonMDMS from "./useMDMS";
-import useModuleTenants from "./useModuleTenants";
 import useQueryParams from "./useQueryParams";
 import useRouteSubscription from "./useRouteSubscription";
-import useSessionStorage from "./useSessionStorage";
 import useStore from "./useStore";
 import { useTenants } from "./useTenants";
 import useCustomAPIHook from "./useCustomAPIHook";
 import { usePrivacyContext } from "./usePrivacyContext";
-import useStaticData from "./useStaticData";
 
 const Hooks = {
-  useSessionStorage,
   useQueryParams,
   useInitStore,
   useClickOutside,
   useBoundaryLocalities,
   useCommonMDMS,
-  useModuleTenants,
   useStore,
   useTenants,
   usePrivacyContext,
   useRouteSubscription,
   useCustomAPIHook,
   useCustomMDMS,
-  useStaticData,
   useLocation,
 };
 
