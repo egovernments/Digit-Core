8b8109c26abad0026ef890dd194cba41e14c3202
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-08-17 14:17:38 +0530

    Update Typography.stories.js

--- frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/atoms/stories/Typography.stories.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/atoms/stories/Typography.stories.js
@@ -2,9 +2,19 @@ import React from "react";
 
 export default {
   title: "Typography",
+  argTypes: {
+    typoName: { control: { type: "select", options: ["text-heading-xl", "text-heading-l","text-heading-m","text-heading-s","text-heading-xs","text-caption-l","text-caption-m","text-caption-s","text-body-l","text-body-s","text-body-xs","text-label","text-link"] } },
+  }
+};
+const Template = (args) => <div className={`typography ${args?.typoName}`} style={args?.style}>Heading XL</div>;
+
+export const Playground = Template.bind({});
+Playground.args = {
+  typoName: "text-heading-xl",
+  style: { color: "inherit" },
 };
 
-export const HeadingXL = () => <div className="typography text-heading-xl">Heading XL</div>;
+export const HeadingXL = () => <div className={`typography text-heading-xl`}>Heading XL</div>;
 export const HeadingL = () => <div className="typography text-heading-l">Heading L</div>;
 export const HeadingM = () => <div className="typography text-heading-m">Heading M</div>;
 export const HeadingS = () => <div className="typography text-heading-s">Heading S</div>;
