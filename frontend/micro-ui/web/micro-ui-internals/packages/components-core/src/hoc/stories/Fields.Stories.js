5690339245c699f08b0f828a7eeea4eb26787c0c
nabeelmd-eGov <nabeel.md@egovernments.com>
2023-08-23 15:12:49 +0530

    field stories

--- frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/hoc/stories/Fields.Stories.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/hoc/stories/Fields.Stories.js
@@ -277,16 +277,3 @@ Playground.args = {
   errors: {},
 };
 
-export const HeadingXL = () => <div className={`typography text-heading-xl`}>Heading XL</div>;
-export const HeadingL = () => <div className="typography text-heading-l">Heading L</div>;
-export const HeadingM = () => <div className="typography text-heading-m">Heading M</div>;
-export const HeadingS = () => <div className="typography text-heading-s">Heading S</div>;
-export const HeadingXS = () => <div className="typography text-heading-xs">Heading XS</div>;
-export const CaptionL = () => <div className="typography text-caption-l">Caption L</div>;
-export const CaptionM = () => <div className="typography text-caption-m">Caption M</div>;
-export const CaptionS = () => <div className="typography text-caption-s">Caption S</div>;
-export const BodyL = () => <div className="typography text-body-l">Body L</div>;
-export const BodyS = () => <div className="typography text-body-s">Body S</div>;
-export const BodyXS = () => <div className="typography text-body-xs">Body XS</div>;
-export const Label = () => <div className="typography text-label">Label</div>;
-export const Link = () => <div className="typography text-link">Link</div>;
