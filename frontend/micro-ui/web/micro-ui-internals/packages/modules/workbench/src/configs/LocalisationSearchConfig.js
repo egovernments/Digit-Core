24c8ccb916ed7d3292b91c835b5696292053cc8f
Nipun Arora <aroranipun1@gmail.com>
2023-08-23 14:41:07 +0530

    Update LocalisationSearchConfig.js

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/workbench/src/configs/LocalisationSearchConfig.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/workbench/src/configs/LocalisationSearchConfig.js
@@ -2,7 +2,7 @@ export const Config = {
   label: "WBH_LOCALISATION_SEARCH_HEADER",
   type: "search",
   actionLabel: "WBH_ADD_LOCALISATION",
-  actionRole: "EMPLOYEE",
+  actionRole: "LOC_ADMIN",
   actionLink: "workbench/localisation-add",
   apiDetails: {
     serviceName: "/localization/messages/v1/_search",
