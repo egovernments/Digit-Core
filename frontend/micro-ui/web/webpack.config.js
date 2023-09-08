927111e2929333f83a860e2b0fd87ce35905e0c0
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-09-08 18:16:22 +0530

    updated the versions

--- frontend/micro-ui/web/webpack.config.js
+++ frontend/micro-ui/web/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   // mode: 'development',
@@ -9,14 +10,9 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js)$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        use: ["babel-loader"],
       },
     ],
   },
@@ -28,10 +24,17 @@ module.exports = {
   optimization: {
     splitChunks: {
       chunks: 'all',
+      minSize:20000,
+      maxSize:50000,
+      enforceSizeThreshold:50000,
+      minChunks:1,
+      maxAsyncRequests:30,
+      maxInitialRequests:30
     },
   },
   plugins: [
     new CleanWebpackPlugin(),
+    // new BundleAnalyzerPlugin(),
     new HtmlWebpackPlugin({ inject: true, template: "public/index.html" }),
   ],
 };
