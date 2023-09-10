170731020edbea6d27075cb3b7f8779054cac528
nabeelmd-eGov <nabeel.md@egovernments.com>
2023-09-10 19:55:22 +0530

    DPG-1714: redefine search form

--- frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/molecules/SearchForm.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/molecules/SearchForm.js
@@ -1,43 +1,14 @@
 import React from "react";
 
 export const SearchField = ({ children, className }) => {
-  const isMobile = window.Digit.Utils.browser.isMobile();
-  const isEnabledCommonModules =
-    window.location.href.includes("/obps/") ||
-    window.location.href.includes("/noc/") ||
-    window.location.href.includes("/ws/water/bill-amendment/inbox") ||
-    window.location.href.includes("/ws/sewerage/bill-amendment/inbox");
-
-  const disbaleModules = window.location.href.includes("obps/search") || window.location.href.includes("noc/search");
-  if (isEnabledCommonModules && !isMobile && !disbaleModules) {
-    return (
-      <div className={`input-fields`} style={{ marginTop: "5px" }}>
-        {children}
-      </div>
-    );
-  }
-  return <div className={`form-field ${className || ""}`}>{children}</div>;
+  // no mission specific code should be written here. for additional define use variant or classname props.
+  return <div className={`digit-form-field ${className || ""}`}>{children}</div>;
 };
 
-export const SearchForm = ({ children, onSubmit, handleSubmit, id, className = "" }) => {
-  const isMobile = window.Digit.Utils.browser.isMobile();
-  const isEnabledCommonModules =
-    window.location.href.includes("/obps/") ||
-    window.location.href.includes("/noc/") ||
-    window.location.href.includes("/ws/water/bill-amendment/inbox") ||
-    window.location.href.includes("/ws/sewerage/bill-amendment/inbox");
-
-  const disbaleModules = window.location.href.includes("obps/search") || window.location.href.includes("noc/search");
-
-  if (isEnabledCommonModules && !isMobile && !disbaleModules) {
-    return (
-      <form onSubmit={handleSubmit(onSubmit)} {...{ id }}>
-        {children}
-      </form>
-    );
-  }
+export const SearchForm = ({ children, onSubmit, handleSubmit, id, className = "", variant = "" }) => {
+  // define variant unset to unset all styles define by classname
   return (
-    <form className={`search-form-wrapper ${className}`} onSubmit={handleSubmit(onSubmit)} {...{ id }}>
+    <form className={`digit-search-form-wrapper ${variant} ${className}`} onSubmit={handleSubmit(onSubmit)} {...{ id }}>
       {children}
     </form>
   );
