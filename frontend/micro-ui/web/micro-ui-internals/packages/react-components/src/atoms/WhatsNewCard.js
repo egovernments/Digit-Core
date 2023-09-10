73428a8feab27de65cb0869171d1248dfc780448
nabeelmd-eGov <nabeel.md@egovernments.com>
2023-09-10 17:48:46 +0530

    DPG-1714: Updated WhatsnewCard component

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/WhatsNewCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/WhatsNewCard.js
@@ -1,20 +1,19 @@
-import React from "react"
-import { useTranslation } from "react-i18next"
+import React from "react";
+import { useTranslation } from "react-i18next";
 
-const WhatsNewCard = ({header, actions, eventNotificationText, timePastAfterEventCreation, timeApproxiamationInUnits}) => {
-    const { t } = useTranslation();
+const WhatsNewCard = ({ header, actions, eventNotificationText, timePastAfterEventCreation, timeApproxiamationInUnits }) => {
+  const { t } = useTranslation();
 
-    const getTransformedLocale = label => {
-        if (typeof label === "number") return label;
-        return label && label.toUpperCase().replace(/[.:-\s\/]/g, "_");
-    };
-
-    return <div className="WhatsNewCard">
-        <h2>{t(header)}</h2>
-        <p>{eventNotificationText}</p>
-        {actions?.map(i => <a href={i?.actionUrl}>{`${t(`CS_COMMON_${getTransformedLocale(i?.code)}`)}`}</a>)}
-        <p>{timePastAfterEventCreation + ` ${t(timeApproxiamationInUnits)}`}</p>
+  return (
+    <div className="digit-whats-new-card">
+      <h2>{t(header)}</h2>
+      <p>{eventNotificationText}</p>
+      {actions?.map((i) => (
+        <a href={i?.actionUrl}>{`${t(`CS_COMMON_${window?.Digit?.Utils?.locale?.getTransformedLocale(i?.code)}`)}`}</a>
+      ))}
+      <p>{timePastAfterEventCreation + ` ${t(timeApproxiamationInUnits)}`}</p>
     </div>
-}
+  );
+};
 
-export default WhatsNewCard
+export default WhatsNewCard;
