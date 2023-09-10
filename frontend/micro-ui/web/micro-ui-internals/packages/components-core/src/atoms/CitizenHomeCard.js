fff104b4ec65db946d1114e6734d0d934f3bb23f
nabeelmd-eGov <nabeel.md@egovernments.com>
2023-09-10 17:56:05 +0530

    DPG-1714: updated citizen home card

--- frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/atoms/CitizenHomeCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/atoms/CitizenHomeCard.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 const CitizenHomeCard = ({ header, links = [], state, Icon, Info, isInfo = false, styles }) => {
   return (
-    <div className="CitizenHomeCard" style={styles ? styles : {}}>
-      <div className="header">
+    <div className="digit-citizen-home-card" style={styles ? styles : {}}>
+      <div className="digit-header">
         <h2>{header}</h2>
         <Icon />
       </div>
 
-      <div className="links">
+      <div className="digit-links">
         {links.map((e, i) => (
-          <div className="linksWrapper">
+          <div className="digit-links-wrapper">
             <Link key={i} to={{ pathname: e.link, state: e.state }}>
               {e.i18nKey}
             </Link>
