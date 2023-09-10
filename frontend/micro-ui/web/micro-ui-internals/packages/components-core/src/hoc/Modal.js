ed5b5cf7d8bf3ebc588ab1c3297bee7ff6a1c441
nabeelmd-eGov <nabeel.md@egovernments.com>
2023-09-10 19:33:36 +0530

    DPG-1714: redefine modal and remove mission specific code with variants

--- frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/hoc/Modal.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/hoc/Modal.js
@@ -22,13 +22,14 @@ const Modal = ({
   style = {},
   popupModuleMianStyles,
   headerBarMainStyle,
-  isOBPSFlow = false,
+  // isOBPSFlow = false,
+  variant = "",
   popupModuleActionBarStyles = {},
 }) => {
   /**
    * TODO: It needs to be done from the desgin changes
    */
-  const mobileView = Digit.Utils.browser.isMobile() ? true : false;
+  // const mobileView = Digit.Utils.browser.isMobile() ? true : false;
   useEffect(() => {
     document.body.style.overflowY = "hidden";
     return () => {
@@ -37,36 +38,21 @@ const Modal = ({
   }, []);
   return (
     <PopUp>
-      <div className="popup-module" style={popupStyles}>
-        {/* <HeaderBar main={headerBarMain} end={headerBarEnd} style={headerBarMainStyle ? headerBarMainStyle : {}} /> */}
-        <Header className="header-wrap" styles={headerBarMainStyle ? headerBarMainStyle : {}}>
-          {headerBarMain ? <div className="header-content">{headerBarMain}</div> : null}
-          {headerBarEnd ? <div className="header-end">{headerBarEnd}</div> : null}
+      <div className="digit-popup-module" style={popupStyles}>
+        <Header className="digit-header-wrap" styles={headerBarMainStyle ? headerBarMainStyle : {}}>
+          {headerBarMain ? <div className="digit-header-content">{headerBarMain}</div> : null}
+          {headerBarEnd ? <div className="digit-header-end">{headerBarEnd}</div> : null}
         </Header>
-        <div className="popup-module-main" style={popupModuleMianStyles ? popupModuleMianStyles : {}}>
+        <div className="digit-popup-module-main" style={popupModuleMianStyles ? popupModuleMianStyles : {}}>
           {children}
           <div
-            className="popup-module-action-bar"
-            style={
-              isOBPSFlow
-                ? !mobileView
-                  ? { marginRight: "18px" }
-                  : { position: "absolute", bottom: "5%", right: "10%", left: window.location.href.includes("employee") ? "0%" : "7%" }
-                : popupModuleActionBarStyles
-            }
+            // V2 -> Instead of mission specific code, we need to pass classnames for addition styling for obps flow i have added classname with -> "digit-variant-obps" .
+            className={`digit-popup-module-action-bar ${variant}`}
+            style={popupModuleActionBarStyles}
           >
-            {actionCancelLabel ? (
-              <Button textStyles={{ margin: "0px" }} theme="border" label={actionCancelLabel} onClick={actionCancelOnSubmit} style={style} />
-            ) : null}
+            {actionCancelLabel ? <Button theme="border" label={actionCancelLabel} onClick={actionCancelOnSubmit} style={style} /> : null}
             {!hideSubmit ? (
-              <Button
-                textStyles={{ margin: "0px" }}
-                label={actionSaveLabel}
-                onClick={actionSaveOnSubmit}
-                formId={formId}
-                isDisabled={isDisabled}
-                style={style}
-              />
+              <Button label={actionSaveLabel} onClick={actionSaveOnSubmit} formId={formId} isDisabled={isDisabled} style={style} />
             ) : null}
           </div>
         </div>
