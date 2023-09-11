4cdbb05015e466d07ba331461cf1d5ceddf2ce41
nabeelmd-eGov <nabeel.md@egovernments.com>
2023-09-11 12:15:26 +0530

    DPG-1714: redefine toastV2

--- frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/atoms/Toast.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/components-core/src/atoms/Toast.js
@@ -4,12 +4,15 @@ import { SVG } from "./SVG";
 import Button from "./Button";
 
 const Toast = (props) => {
+  const variant = props?.error ? "digit-error" : props?.warning ? "digit-warning" : props?.variant ? props?.variant : "";
+  const isWarningButtons = props?.isWarningButtons ? "digit-warning-buttons" : "";
+
   if (props.error) {
     return (
-      <div className="toast-success" style={{ backgroundColor: "red", ...props.style }}>
-        <SVG.Error />
-        <h2 style={{...props.labelstyle}}>{props.label}</h2>
-        { props.isDleteBtn ? <SVG.Delete fill="none" className="toast-close-btn" onClick={props.onClose} /> : null }
+      <div className={`digit-toast-success ${variant}`} style={{ ...props.style }}>
+        <SVG.Error fill="#fff" />
+        <h2 style={{ ...props.labelstyle }}>{props.label}</h2>
+        {props.isDleteBtn ? <SVG.Delete fill="none" className="digit-toast-close-btn" onClick={props.onClose} /> : null}
       </div>
     );
   }
@@ -17,34 +20,36 @@ const Toast = (props) => {
   if (props.warning) {
     return (
       <div>
-        <div className="toast-success" style={props?.isWarningButtons ? { backgroundColor: "#EA8A3B", display: "block", ...props.style } : { backgroundColor: "#EA8A3B", ...props.style }}>
-          {!props?.isWarningButtons ?
-            <div className="toast-success" style={{ backgroundColor: "#EA8A3B", ...props.style }}>
-              <SVG.Error />
-              <h2 style={{ marginLeft: "10px" }}>{props.label}</h2>
-              {props.isDleteBtn ? <SVG.Delete fill="none" className="toast-close-btn" onClick={props.onClose} /> : null}
-            </div> : <div style={{ display: "flex" }}>
-              <SVG.Error />
-              <h2 style={{ marginLeft: "10px" }}>{props.label}</h2>
-              {props.isDleteBtn ? <SVG.Delete fill="none" className="toast-close-btn" onClick={props.onClose} /> : null}
+        <div className={`digit-toast-success ${variant} ${isWarningButtons}`} style={{ ...props.style }}>
+          {!props?.isWarningButtons ? (
+            <div className={`digit-toast-success ${variant} ${isWarningButtons}`} style={{ ...props.style }}>
+              <SVG.Error fill="#fff" />
+              <h2>{props.label}</h2>
+              {props.isDleteBtn ? <SVG.Delete fill="none" className="digit-toast-close-btn" onClick={props.onClose} /> : null}
             </div>
-          }
-          {props?.isWarningButtons ?
-            <div style={{ width: "100%", display: "flex", justifyContent: "flex-end" }}>
-              <Button theme="border" label={"NO"} onClick={props.onNo} style={{ marginLeft: "10px" }} />
-              <Button label={"YES"} onClick={props.onYes} style={{ marginLeft: "10px" }} />
-            </div> : null
-          }
+          ) : (
+            <div className="digit-toast-sub-container">
+              <SVG.Error fill="#fff" />
+              <h2>{props.label}</h2>
+              {props.isDleteBtn ? <SVG.Delete fill="none" className="digit-toast-close-btn" onClick={props.onClose} /> : null}
+            </div>
+          )}
+          {props?.isWarningButtons ? (
+            <div className="digit-warning-button-container">
+              <Button theme="border" label={"NO"} onClick={props.onNo} />
+              <Button label={"YES"} onClick={props.onYes} />
+            </div>
+          ) : null}
         </div>
       </div>
     );
   }
 
   return (
-    <div className="toast-success" style={{ ...props.style }}>
+    <div className="digit-toast-success" style={{ ...props.style }}>
       <SVG.CheckCircle />
       <h2>{props.label}</h2>
-      <SVG.Delete fill="none" className="toast-close-btn" onClick={props.onClose} />
+      <SVG.Delete fill="none" className="digit-toast-close-btn" onClick={props.onClose} />
     </div>
   );
 };
@@ -52,13 +57,13 @@ const Toast = (props) => {
 Toast.propTypes = {
   label: PropTypes.string,
   onClose: PropTypes.func,
-  isDleteBtn: PropTypes.bool
+  isDleteBtn: PropTypes.bool,
 };
 
 Toast.defaultProps = {
   label: "",
   onClose: undefined,
-  isDleteBtn: false
+  isDleteBtn: false,
 };
 
 export default Toast;
