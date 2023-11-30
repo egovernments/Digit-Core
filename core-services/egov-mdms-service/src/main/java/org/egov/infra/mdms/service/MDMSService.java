604f005cc85b828d4baa5c6ffd632e6133be2dc2
Jagankumar <53823168+jagankumar-egov@users.noreply.github.com>
2023-11-30 11:13:10 +0530

    Fixed that fallback logic of the MDMS v1 Api

--- core-services/egov-mdms-service/src/main/java/org/egov/infra/mdms/service/MDMSService.java
+++ core-services/egov-mdms-service/src/main/java/org/egov/infra/mdms/service/MDMSService.java
@@ -1,7 +1,6 @@
 package org.egov.infra.mdms.service;
 
 import java.util.*;
-
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.jayway.jsonpath.JsonPath;
 import org.egov.common.utils.MultiStateInstanceUtil;
@@ -14,30 +13,24 @@ import org.egov.infra.mdms.utils.SchemaUtil;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONArray;
 import org.springframework.util.ObjectUtils;
-
 import static org.egov.infra.mdms.utils.MDMSConstants.*;
 
 @Service
 @Slf4j
 public class MDMSService {
-
 	private MdmsDataValidator mdmsDataValidator;
-
 	private MdmsDataEnricher mdmsDataEnricher;
-
 	private MdmsDataRepository mdmsDataRepository;
-
 	private SchemaUtil schemaUtil;
-
 	private MultiStateInstanceUtil multiStateInstanceUtil;
 
 	@Autowired
 	public MDMSService(MdmsDataValidator mdmsDataValidator, MdmsDataEnricher mdmsDataEnricher,
-					   MdmsDataRepository mdmsDataRepository, SchemaUtil schemaUtil, MultiStateInstanceUtil multiStateInstanceUtil) {
+			MdmsDataRepository mdmsDataRepository, SchemaUtil schemaUtil,
+			MultiStateInstanceUtil multiStateInstanceUtil) {
 		this.mdmsDataValidator = mdmsDataValidator;
 		this.mdmsDataEnricher = mdmsDataEnricher;
 		this.mdmsDataRepository = mdmsDataRepository;
@@ -47,73 +40,97 @@ public class MDMSService {
 
 	/**
 	 * This method processes the requests that come for master data creation.
+	 *
 	 * @param mdmsRequest
 	 * @return
 	 */
 	public List<Mdms> create(MdmsRequest mdmsRequest) {
-
 		// Fetch schema against which data is getting created
 		JSONObject schemaObject = schemaUtil.getSchema(mdmsRequest);
-
 		// Validate incoming request
 		mdmsDataValidator.validateCreateRequest(mdmsRequest, schemaObject);
-
 		// Enrich incoming request
 		mdmsDataEnricher.enrichCreateRequest(mdmsRequest, schemaObject);
-
 		// Emit mdms creation request event
 		mdmsDataRepository.create(mdmsRequest);
-
 		return Arrays.asList(mdmsRequest.getMdms());
 	}
 
 	/**
 	 * This method processes the requests that come for master data search.
+	 *
 	 * @param mdmsCriteriaReq
 	 * @return
 	 */
 	public Map<String, Map<String, JSONArray>> search(MdmsCriteriaReq mdmsCriteriaReq) {
-		Map<String, Map<String, JSONArray>> tenantMasterMap = new HashMap<>();
-
+		final Map<String, Map<String, JSONArray>> tenantMasterMap = new HashMap<>();
 		/*
-		 * Set incoming tenantId as state level tenantId for fallback in case master data for
+		 * Set incoming tenantId as state level tenantId for fallback in case master
+		 * data for
 		 * concrete tenantId does not exist.
 		 */
 		String tenantId = new StringBuilder(mdmsCriteriaReq.getMdmsCriteria().getTenantId()).toString();
 		mdmsCriteriaReq.getMdmsCriteria().setTenantId(multiStateInstanceUtil.getStateLevelTenant(tenantId));
-
 		Map<String, String> schemaCodes = getSchemaCodes(mdmsCriteriaReq.getMdmsCriteria());
 		mdmsCriteriaReq.getMdmsCriteria().setSchemaCodeFilterMap(schemaCodes);
+		schemaCodes.entrySet().forEach(entry -> {
+			String schemaCodeStr = entry.getKey();
+			String schemaCodeFilterStr = entry.getValue();
+			MdmsCriteria mdmsNewCriteria = new MdmsCriteria();
+			mdmsNewCriteria.setTenantId(mdmsCriteriaReq.getMdmsCriteria().getTenantId());
+			mdmsNewCriteria.setIsActive(mdmsCriteriaReq.getMdmsCriteria().getIsActive());
+			Map<String, String> newSchemaCodesMap = new HashMap<>();
+			newSchemaCodesMap.put(schemaCodeStr, schemaCodeFilterStr);
+			mdmsNewCriteria.setSchemaCodeFilterMap(newSchemaCodesMap);
+			Map<String, Map<String, JSONArray>> tenantMasterNewMap = new HashMap<>();
+			tenantMasterNewMap = mdmsDataRepository.search(mdmsNewCriteria);
+			tenantMasterNewMap = applyFilterToData(tenantMasterNewMap,
+					mdmsCriteriaReq.getMdmsCriteria().getSchemaCodeFilterMap());
 
-		// Make a call to the repository layer to fetch data as per given criteria
-		tenantMasterMap = mdmsDataRepository.search(mdmsCriteriaReq.getMdmsCriteria());
+			Map<String, Map<String, JSONArray>> finalTenantMasterMap = new HashMap<>();
+			for (Map.Entry<String, Map<String, JSONArray>> map : tenantMasterNewMap.entrySet()) {
+				Map<String, JSONArray> newHashMap = new HashMap<>();
+				newHashMap.putAll(map.getValue());
+				finalTenantMasterMap.put(mdmsCriteriaReq.getMdmsCriteria().getTenantId(), newHashMap);
+			}
 
-		// Apply filters to incoming data
-		tenantMasterMap = applyFilterToData(tenantMasterMap, mdmsCriteriaReq.getMdmsCriteria().getSchemaCodeFilterMap());
+			Map<String, JSONArray> newHashMap = new HashMap<>();
+
+			if (!tenantMasterNewMap.isEmpty()
+					&& !tenantMasterNewMap.get(mdmsCriteriaReq.getMdmsCriteria().getTenantId()).isEmpty()) {
+				newHashMap.putAll(tenantMasterNewMap.get(mdmsCriteriaReq.getMdmsCriteria().getTenantId()));
+			}
+
+			if (tenantMasterMap.isEmpty()
+					|| tenantMasterMap.get(mdmsCriteriaReq.getMdmsCriteria().getTenantId()).isEmpty()) {
+				tenantMasterMap.put(mdmsCriteriaReq.getMdmsCriteria().getTenantId(), newHashMap);
+			} else {
+				tenantMasterMap.get(mdmsCriteriaReq.getMdmsCriteria().getTenantId()).putAll(newHashMap);
+			}
+		});
 
 		// Perform fallback
 		Map<String, JSONArray> masterDataMap = FallbackUtil.backTrackTenantMasterDataMap(tenantMasterMap, tenantId);
-
 		// Return response in MDMS v1 search response format for backward compatibility
 		return getModuleMasterMap(masterDataMap);
 	}
 
-	private Map<String, Map<String, JSONArray>> applyFilterToData(Map<String, Map<String, JSONArray>> tenantMasterMap, Map<String, String> schemaCodeFilterMap) {
+	private Map<String, Map<String, JSONArray>> applyFilterToData(Map<String, Map<String, JSONArray>> tenantMasterMap,
+			Map<String, String> schemaCodeFilterMap) {
 		Map<String, Map<String, JSONArray>> tenantMasterMapPostFiltering = new HashMap<>();
-
 		tenantMasterMap.keySet().forEach(tenantId -> {
 			Map<String, JSONArray> schemaCodeVsFilteredMasters = new HashMap<>();
 			tenantMasterMap.get(tenantId).keySet().forEach(schemaCode -> {
 				JSONArray masters = tenantMasterMap.get(tenantId).get(schemaCode);
-				if(!ObjectUtils.isEmpty(schemaCodeFilterMap.get(schemaCode))) {
-					schemaCodeVsFilteredMasters.put(schemaCode, filterMasters(masters, schemaCodeFilterMap.get(schemaCode)));
+				if (!ObjectUtils.isEmpty(schemaCodeFilterMap.get(schemaCode))) {
+					schemaCodeVsFilteredMasters.put(schemaCode,
+							filterMasters(masters, schemaCodeFilterMap.get(schemaCode)));
 				} else {
 					schemaCodeVsFilteredMasters.put(schemaCode, masters);
 				}
 			});
 			tenantMasterMapPostFiltering.put(tenantId, schemaCodeVsFilteredMasters);
 		});
-
 		return tenantMasterMapPostFiltering;
 	}
 
@@ -124,12 +141,10 @@ public class MDMSService {
 
 	private Map<String, Map<String, JSONArray>> getModuleMasterMap(Map<String, JSONArray> masterMap) {
 		Map<String, Map<String, JSONArray>> moduleMasterMap = new HashMap<>();
-
 		for (Map.Entry<String, JSONArray> entry : masterMap.entrySet()) {
 			String[] moduleMaster = entry.getKey().split(DOT_REGEX);
 			String moduleName = moduleMaster[0];
 			String masterName = moduleMaster[1];
-
 			moduleMasterMap.computeIfAbsent(moduleName, k -> new HashMap<>())
 					.put(masterName, entry.getValue());
 		}
