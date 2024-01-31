107df5273966b4171b9cc966cb40899ec7aea27a
talele08 <talele.aniket@gmail.com>
2024-01-31 11:32:25 +0530

    Temporary changes in tracer for springboot 3.0 upgrade

--- core-services/libraries/tracer/src/main/java/org/egov/tracer/model/ErrorRes.java
+++ core-services/libraries/tracer/src/main/java/org/egov/tracer/model/ErrorRes.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.egov.common.contract.response.ResponseInfo;
-
 import java.util.List;
 
 /**
