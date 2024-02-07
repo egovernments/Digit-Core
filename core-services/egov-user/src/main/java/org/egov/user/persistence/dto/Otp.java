aa283f38c0c201b20ac197971f133814c1ed6dc4
Shashwat Mishra <shashwat.mishra@egovernments.org>
2024-02-07 16:14:33 +0530

    [CORE-2.9-LTS] Made fixes in user service model and updated pom to point to upgraded libraries

--- core-services/egov-user/src/main/java/org/egov/user/persistence/dto/Otp.java
+++ core-services/egov-user/src/main/java/org/egov/user/persistence/dto/Otp.java
@@ -4,10 +4,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Otp {
 
     @JsonProperty("isValidationSuccessful")
