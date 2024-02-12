d1a2bcbd2c4c192df9d2cc85ffa6075769c0f592
Rishabh-egov <rishabh.jain@egovernments.org>
2024-02-12 17:11:23 +0530

    [CORE-2.9-LTS] removed missingMigration flag in - access-control - filestore - localization - location - wf-v2 - service-req

--- core-services/egov-workflow-v2/src/main/resources/db/migrate.sh
+++ core-services/egov-workflow-v2/src/main/resources/db/migrate.sh
@@ -7,5 +7,5 @@ echo "the schemas : $schemas"
 for schemaname in ${schemas//,/ }
 do
     echo "the schema name : ${baseurl}${schemasetter}${schemaname}"
-    flyway -url=${baseurl}${schemasetter}${schemaname}  -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true -outOfOrder=true -ignoreMissingMigrations=true migrate
+    flyway -url=${baseurl}${schemasetter}${schemaname}  -table=$SCHEMA_TABLE -user=$FLYWAY_USER -password=$FLYWAY_PASSWORD -locations=$FLYWAY_LOCATIONS -baselineOnMigrate=true -outOfOrder=true migrate
 done
