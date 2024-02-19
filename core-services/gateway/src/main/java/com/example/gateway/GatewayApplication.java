6f2ef27a2d8675f26454cbbaddff827ed3aec323
Rishabh-egov <rishabh.jain@egovernments.org>
2024-02-19 17:16:10 +0530

    [CORE-2.9-LTS] Shifted routes to routes.properties and conigured the same

--- core-services/gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ core-services/gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,6 +13,7 @@ import java.util.List;
 
 @SpringBootApplication
 @RestController
+@PropertySource({"${spring.routes.filepath}","${spring.limiter.filepath}"})
 public class GatewayApplication {
 
 	public static void main(String[] args) {
