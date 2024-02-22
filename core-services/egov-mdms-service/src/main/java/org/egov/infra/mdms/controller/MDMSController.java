bb0fdf50da7956f17cd66c05d659c6aaec3b8b22
debasishchakraborty-egovt <debasish@egovernmets.org>
2024-02-22 18:23:13 +0530

    PFM-5666: added /_get api in mdms-v2 service

--- core-services/egov-mdms-service/src/main/java/org/egov/infra/mdms/controller/MDMSController.java
+++ core-services/egov-mdms-service/src/main/java/org/egov/infra/mdms/controller/MDMSController.java
@@ -1,5 +1,6 @@
 package org.egov.infra.mdms.controller;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import javax.validation.Valid;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.egov.common.contract.request.RequestInfo;
 import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONArray;
 
@@ -36,6 +38,43 @@ public class MDMSController {
                 .mdmsRes(moduleMasterMap)
                 .build();
         return new ResponseEntity<>(mdmsResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("_get")
+    @ResponseBody
+    private ResponseEntity<?> search(@RequestParam("moduleName") String module,
+                                     @RequestParam("masterName") String master,
+                                     @RequestParam(value = "filter", required = false) String filter,
+                                     @RequestParam("tenantId") String tenantId,
+                                     @RequestBody RequestInfo requestInfo) {
+
+        log.info("MDMSController mdmsCriteriaReq [" + module + ", " + master + ", " + filter + "]");
+        MdmsCriteriaReq mdmsCriteriaReq = new MdmsCriteriaReq();
+        mdmsCriteriaReq.setRequestInfo(requestInfo);
+        MdmsCriteria criteria = new MdmsCriteria();
+        criteria.setTenantId(tenantId);
+
+        ModuleDetail detail = new ModuleDetail();
+        detail.setModuleName(module);
+
+        MasterDetail masterDetail = new MasterDetail();
+        masterDetail.setName(master);
+        masterDetail.setFilter(filter);
+        ArrayList<MasterDetail> masterList = new ArrayList<>();
+        masterList.add(masterDetail);
+        detail.setMasterDetails(masterList);
+
+        ArrayList<ModuleDetail> moduleList = new ArrayList<>();
+        moduleList.add(detail);
+
+        criteria.setModuleDetails(moduleList);
+        mdmsCriteriaReq.setMdmsCriteria(criteria);
+
+        Map<String, Map<String, JSONArray>> response = mdmsService.search(mdmsCriteriaReq);
+        MdmsResponse mdmsResponse = new MdmsResponse();
+        mdmsResponse.setMdmsRes(response);
+        return new ResponseEntity<>(mdmsResponse, HttpStatus.OK);
+
     }
 
 }
