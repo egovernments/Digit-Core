76ab0125ee24c507d799ed88fc884ce0004195a6
debasishchakraborty-egovt <debasish@egovernmets.org>
2024-02-23 13:20:48 +0530

    PFM-5666: added /_get api in mdms-v2 service

--- core-services/egov-mdms-service/src/main/java/org/egov/infra/mdms/controller/MDMSController.java
+++ core-services/egov-mdms-service/src/main/java/org/egov/infra/mdms/controller/MDMSController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.egov.common.contract.request.RequestInfo;
 import lombok.extern.slf4j.Slf4j;
 import net.minidev.json.JSONArray;
 
