6de748e735f88a90cf6c8543d730a99241836e45
mithun-hegde-egov <mithun.hegde@egovernments.org>
2024-05-08 12:22:23 +0530

    ignore request info

--- accelerators/sunbirdrc-credential-service/src/main/java/org/egov/sunbirdrc/models/MdmsSchema.java
+++ accelerators/sunbirdrc-credential-service/src/main/java/org/egov/sunbirdrc/models/MdmsSchema.java
@@ -1,5 +1,6 @@
 package org.egov.sunbirdrc.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,7 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class MdmsSchema {
     private MdmsData mdmsData;
     private Schema schema;
