5fbf735c6a29241b0358e15a71b80d842769a30f
mithun-hegde-egov <mithun.hegde@egovernments.org>
2024-06-06 15:39:44 +0530

    removing the ui code

--- accelerators/verification-ui/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  rootDir: './',
-  testPathIgnorePatterns: ["<rootDir>/node_modules"],
-  testMatch: ["<rootDir>/src/**/*.test.ts"],
-  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"]
-};
--- accelerators/verification-ui/src/components/Home/VerificationProgressTracker/InjiStepper/StepperConnector.tsx
+++
@@ -1,5 +0,0 @@
-import styled from "@emotion/styled";
-
-export const StepperConnector = styled('div')`
-    border-color: #FF8F00;
-`
