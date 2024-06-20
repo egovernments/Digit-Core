fab03b94d3bc306b420ca2198fc392958706cb78
mithun-hegde-egov <mithun.hegde@egovernments.org>
2024-06-20 11:36:48 +0530

    removed the directory

--- accelerators/verification-ui/src/types/function-types.ts
+++
@@ -1,7 +0,0 @@
-import {AlertInfo, QrScanResult} from "./data-types";
-
-export type SetActiveStepFunction = (activeStep: number) => void;
-export type SetVcStatusFunction = (vc: any) => void;
-export type SetQrDataFunction = (qrData: string) => void;
-export type SetScanResultFunction = (result: QrScanResult) => void;
-export type SetAlertInfoFunction = ((alert: AlertInfo) => void) | ((setUsingCurrentAlert: ((alert: AlertInfo) => AlertInfo)) => void);
