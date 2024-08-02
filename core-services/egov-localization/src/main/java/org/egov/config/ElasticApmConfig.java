c4c6a70a67fd596e4d74d3d4489a0d4feae25d62
varunreddy-egov <varun.kumar@egovernments.org>
2024-08-02 10:52:07 +0530

    Added configuration for elastic-apm-agent

---
+++ core-services/egov-localization/src/main/java/org/egov/config/ElasticApmConfig.java
@@ -0,0 +1,50 @@
+package org.egov.config;
+
+import co.elastic.apm.attach.ElasticApmAttacher;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "elastic.apm")
+@ConditionalOnProperty(value = "elastic.apm.enabled", havingValue = "true")
+public class ElasticApmConfig {
+
+    private static final String SERVER_URL_KEY = "server_url";
+    private String serverUrl;
+
+    private static final String SERVICE_NAME_KEY = "service_name";
+    private String serviceName;
+
+    private static final String SECRET_TOKEN_KEY = "secret_token";
+    private String secretToken;
+
+    private static final String ENVIRONMENT_KEY = "environment";
+    private String environment;
+
+    private static final String APPLICATION_PACKAGES_KEY = "application_packages";
+    private String applicationPackages;
+
+    private static final String LOG_LEVEL_KEY = "log_level";
+    private String logLevel;
+
+    @PostConstruct
+    public void init() {
+
+        Map<String, String> apmProps = new HashMap<>(6);
+        apmProps.put(SERVER_URL_KEY, serverUrl);
+        apmProps.put(SERVICE_NAME_KEY, serviceName);
+        apmProps.put(SECRET_TOKEN_KEY, secretToken);
+        apmProps.put(ENVIRONMENT_KEY, environment);
+        apmProps.put(APPLICATION_PACKAGES_KEY, applicationPackages);
+        apmProps.put(LOG_LEVEL_KEY, logLevel);
+
+        ElasticApmAttacher.attach(apmProps);
+    }
+}
