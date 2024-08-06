90afd892de512d8f779ddc3e038c721ab1662101
Rishabh-egov <137171925+Rishabh-egov@users.noreply.github.com>
2024-08-06 17:26:47 +0530

    Fixed javax

--- core-services/egov-localization/src/main/java/org/egov/ElasticApmConfig.java
+++ core-services/egov-localization/src/main/java/org/egov/ElasticApmConfig.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
