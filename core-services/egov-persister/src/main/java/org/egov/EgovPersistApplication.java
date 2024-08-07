06e7220bcc365d2ba2c6fa7fd2cd535274b7c545
varunreddy-egov <varun.kumar@egovernments.org>
2024-08-07 16:29:05 +0530

    Added Elastic APM Agent to persister.

--- core-services/egov-persister/src/main/java/org/egov/EgovPersistApplication.java
+++ core-services/egov-persister/src/main/java/org/egov/EgovPersistApplication.java
@@ -1,5 +1,6 @@
 package org.egov;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -37,9 +38,31 @@ public class EgovPersistApplication {
     @Autowired
     private ApplicationContext applicationContext;
 
+    @Value("${elastic.apm.service-name}")
+    private String serviceName;
+
+    @Value("${elastic.apm.server-url}")
+    private String serviceUrl;
+
+    @Value("${elastic.apm.application-packages}")
+    private String applicationPackages;
+
+    @Value("${elastic.apm.environment}")
+    private String environment;
 
     public static void main(String[] args) {
         SpringApplication.run(EgovPersistApplication.class, args);
+    }
+
+    @PostConstruct
+    public void initElasticApm() {
+        Map<String, String> apmConfig = new HashMap<>();
+        apmConfig.put("service_name", serviceName);
+        apmConfig.put("server_urls", serviceUrl);
+        apmConfig.put("secret_token", "");
+        apmConfig.put("application_packages", applicationPackages);
+        apmConfig.put("environment", environment);
+        ElasticApmAttacher.attach(apmConfig);
     }
 
     //file types to be resolved have to be passed as comma separated types.
