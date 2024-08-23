516b4077627024ff3aa0c9b8dbf20692c58585b8
Rishabh-egov <137171925+Rishabh-egov@users.noreply.github.com>
2024-08-23 17:05:49 +0530

    fixed jsonproperty

--- core-services/user-otp/src/main/java/org/egov/persistence/contract/Email.java
+++ core-services/user-otp/src/main/java/org/egov/persistence/contract/Email.java
@@ -1,5 +1,6 @@
 package org.egov.persistence.contract;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
