37fe9a6e11704da16096445665d24f2bd0c4e452
nikhilmulinti <nikhil.mulinti@egovernments.org>
2024-09-10 14:25:12 +0530

    configuring open telemetry for mdms

--- build/maven/start.sh
+++ build/maven/start.sh
@@ -8,4 +8,6 @@ if [ x"${JAVA_ENABLE_DEBUG}" != x ] && [ "${JAVA_ENABLE_DEBUG}" != "false" ]; th
     java_debug_args="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${JAVA_DEBUG_PORT:-5005}"
 fi
 
+export JAVA_OPTS="${JAVA_OPTS} -javaagent:/opt/egov/opentelemetry-javaagent.jar"
+
 exec java ${java_debug_args} ${JAVA_OPTS} ${JAVA_ARGS}  -jar /opt/egov/*.jar
