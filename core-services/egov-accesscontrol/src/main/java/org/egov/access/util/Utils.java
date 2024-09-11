588d1eb848345aae99e15960a019c2d60979151e
Rishabh-egov <rishabh.jain@egovernments.org>
2024-09-11 12:14:55 +0530

    Added wildcard for access control

--- core-services/egov-accesscontrol/src/main/java/org/egov/access/util/Utils.java
+++ core-services/egov-accesscontrol/src/main/java/org/egov/access/util/Utils.java
@@ -2,24 +2,29 @@ package org.egov.access.util;
 
 public class Utils {
 
-    private Utils(){}
+    private Utils() {
+    }
 
     private static final String OPENING_BRACES = "{";
     private static final String CLOSING_BRACES = "}";
     private static final String PARAMETER_PLACEHOLDER_REGEX = "\\{\\w+\\}";
+    private static final String PARAMETER_DOUBLE_ASTERISK = "**";
     private static final String ANY_WORD_REGEX = "\\\\w+";
+    private static final String WILDCARD_DOUBLE_ASTERISK_REGEX = ".*"; // To match any sequence of characters
 
 
     public static boolean isRegexUri(String url) {
-        return url.contains(OPENING_BRACES) & url.contains(CLOSING_BRACES);
+        return url.contains(OPENING_BRACES) & url.contains(CLOSING_BRACES) || url.contains(PARAMETER_DOUBLE_ASTERISK);
     }
 
-    public static boolean isRegexUriMatch(String actionUri, String requestUri){
+    public static boolean isRegexUriMatch(String actionUri, String requestUri) {
 
         return requestUri.matches(getRegexUri(actionUri));
     }
 
     private static String getRegexUri(String url) {
-        return url.replaceAll(PARAMETER_PLACEHOLDER_REGEX, ANY_WORD_REGEX);
+        String regexUrl = url.replaceAll(PARAMETER_PLACEHOLDER_REGEX, ANY_WORD_REGEX);
+        regexUrl = regexUrl.replace(PARAMETER_DOUBLE_ASTERISK, WILDCARD_DOUBLE_ASTERISK_REGEX);
+        return regexUrl;
     }
 }
