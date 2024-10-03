e957eea611d9a988eb9843b31acd2912c5b7d9eb
Shashwat Mishra <shashwat.mishra@egovernments.org>
2024-10-03 10:56:36 +0530

    Adding property such that producer bean initializes only if decryption audit is enabled

--- core-services/libraries/enc-client/src/main/java/org/egov/encryption/producer/Producer.java
+++ core-services/libraries/enc-client/src/main/java/org/egov/encryption/producer/Producer.java
@@ -2,9 +2,11 @@ package org.egov.encryption.producer;
 
 import org.egov.tracer.kafka.CustomKafkaTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "decryption.audit.enabled", havingValue = "true", matchIfMissing = false)
 public class Producer {
 
     @Autowired
