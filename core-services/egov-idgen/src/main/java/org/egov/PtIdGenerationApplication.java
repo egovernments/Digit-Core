200d09385b474d5c28dfd9a4ef73fd90df79f277
varunreddy-egov <varun.kumar@egovernments.org>
2024-12-30 16:23:13 +0530

    Integrated jaeger in idgen

--- core-services/egov-idgen/src/main/java/org/egov/PtIdGenerationApplication.java
+++ core-services/egov-idgen/src/main/java/org/egov/PtIdGenerationApplication.java
@@ -1,13 +1,16 @@
 package org.egov;
 
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import org.egov.tracer.config.TracerConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 /**
  * Description : This is initialization class for pt-idGeneration module
- * 
+ *
  * @author Pavan Kumar Kamma
  *
  */
@@ -16,5 +19,10 @@ import org.springframework.context.annotation.Import;
 public class PtIdGenerationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PtIdGenerationApplication.class, args);
+	}
+
+	@Bean
+	public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
+		return OtlpGrpcSpanExporter.builder().setEndpoint(url).build();
 	}
 }
