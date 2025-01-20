a47bbd731ab2c5bb4af73e87a8e83c1cb8af3e8f
varunreddy-egov <varun.kumar@egovernments.org>
2025-01-20 16:18:40 +0530

    Modified RestTemplate bean initialization

--- core-services/libraries/tracer/src/main/java/org/egov/tracer/config/TracerConfiguration.java
+++ core-services/libraries/tracer/src/main/java/org/egov/tracer/config/TracerConfiguration.java
@@ -4,17 +4,16 @@ import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
 import io.opentracing.noop.NoopTracerFactory;
 import org.egov.tracer.http.RestTemplateLoggingInterceptor;
 import org.egov.tracer.http.filters.TracerFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.beans.factory.annotation.Value;
-
-import java.util.Collections;
 
 @Configuration
 @EnableAspectJAutoProxy
@@ -24,60 +23,62 @@ import java.util.Collections;
 //@Import(OpenTracingConfiguration.class)
 public class TracerConfiguration {
 
-    @Bean
-    public ObjectMapperFactory objectMapperFactory(TracerProperties tracerProperties, Environment environment) {
-        return new ObjectMapperFactory(tracerProperties, environment);
-    }
+	@Bean
+	public ObjectMapperFactory objectMapperFactory(TracerProperties tracerProperties, Environment environment) {
+		return new ObjectMapperFactory(tracerProperties, environment);
+	}
 
-    @Bean(name = "logAwareRestTemplate")
-    public RestTemplate logAwareRestTemplate(TracerProperties tracerProperties) {
-        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
-        requestFactory.setOutputStreaming(false);
-        RestTemplate restTemplate =
-                new RestTemplate(new BufferingClientHttpRequestFactory(requestFactory));
-        restTemplate.setInterceptors(Collections.singletonList(new RestTemplateLoggingInterceptor(tracerProperties)));
-        return restTemplate;
-    }
+	@Bean(name = "logAwareRestTemplate")
+	public RestTemplate logAwareRestTemplate(RestTemplateBuilder builder, TracerProperties tracerProperties) {
+		return builder
+				.requestFactory(() -> {
+					SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+					factory.setOutputStreaming(false);
+					return new BufferingClientHttpRequestFactory(factory);
+				})
+				.interceptors(new RestTemplateLoggingInterceptor(tracerProperties))
+				.build();
+	}
 
-    /**
-     * Configure tracer filter with order one
-     *
-     * @param objectMapperFactory Object mapper
-     * @param tracerProperties    configuration for the filter
-     * @return Filter
-     */
-    @Bean
-    @ConditionalOnProperty(name = "tracer.filter.enabled",
-            havingValue = "true", matchIfMissing = true)
-    public FilterRegistrationBean tracerFilter(ObjectMapperFactory objectMapperFactory,
-                                               TracerProperties tracerProperties) {
-        final TracerFilter tracerFilter = new TracerFilter(tracerProperties, objectMapperFactory);
-        FilterRegistrationBean registration = new FilterRegistrationBean(tracerFilter);
-        registration.addUrlPatterns("/*");
-        registration.setName("TracerFilter");
-        registration.setOrder(1);
-        return registration;
-    }
+	/**
+	 * Configure tracer filter with order one
+	 *
+	 * @param objectMapperFactory Object mapper
+	 * @param tracerProperties    configuration for the filter
+	 * @return Filter
+	 */
+	@Bean
+	@ConditionalOnProperty(name = "tracer.filter.enabled",
+			havingValue = "true", matchIfMissing = true)
+	public FilterRegistrationBean tracerFilter(ObjectMapperFactory objectMapperFactory,
+											   TracerProperties tracerProperties) {
+		final TracerFilter tracerFilter = new TracerFilter(tracerProperties, objectMapperFactory);
+		FilterRegistrationBean registration = new FilterRegistrationBean(tracerFilter);
+		registration.addUrlPatterns("/*");
+		registration.setName("TracerFilter");
+		registration.setOrder(1);
+		return registration;
+	}
 
-    /**
-     * Disable open tracing by injecting a Noop
-     *
-     * @return Noop tracer
-     */
-    @Bean
-    @ConditionalOnProperty(
-            name = {"tracer.opentracing.enabled"},
-            havingValue = "false",
-            matchIfMissing = true
-    )
-    public io.opentracing.Tracer tracer() {
-        return NoopTracerFactory.create();
-    }
+	/**
+	 * Disable open tracing by injecting a Noop
+	 *
+	 * @return Noop tracer
+	 */
+	@Bean
+	@ConditionalOnProperty(
+			name = {"tracer.opentracing.enabled"},
+			havingValue = "false",
+			matchIfMissing = true
+	)
+	public io.opentracing.Tracer tracer() {
+		return NoopTracerFactory.create();
+	}
 
-    @Bean
-    public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
-        return OtlpGrpcSpanExporter.builder().setEndpoint(url).build();
-    }
+	@Bean
+	public OtlpGrpcSpanExporter otlpHttpSpanExporter(@Value("${tracing.url}") String url) {
+		return OtlpGrpcSpanExporter.builder().setEndpoint(url).build();
+	}
 }
 
 
