e46200db4aa4422ffb564bf4486ec03e9a95c4b2
varunreddy-egov <varun.kumar@egovernments.org>
2025-01-23 12:27:26 +0530

    Built user from JWT token

--- core-services/gateway/src/main/java/com/example/gateway/filters/pre/JwtToHeaderGlobalFilter.java
+++ core-services/gateway/src/main/java/com/example/gateway/filters/pre/JwtToHeaderGlobalFilter.java
@@ -1,5 +1,12 @@
 package com.example.gateway.filters.pre;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.egov.common.contract.request.Role;
+import org.egov.common.contract.request.User;
+import org.slf4j.MDC;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.http.server.reactive.ServerHttpRequest;
@@ -9,8 +16,17 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+import java.util.Map;
+
+import static com.example.gateway.constants.GatewayConstants.USER_INFO_KEY;
+
 @Component
+@Slf4j
 public class JwtToHeaderGlobalFilter implements GlobalFilter {
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
@@ -19,6 +35,12 @@ public class JwtToHeaderGlobalFilter implements GlobalFilter {
             if (principal instanceof Jwt) {
                 Jwt jwt = (Jwt) principal;
                 String userId = jwt.getClaim("sid");
+                User user = buildUserFromJwt(jwt);
+                try {
+                    MDC.put(USER_INFO_KEY, objectMapper.writeValueAsString(user));
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
                 ServerHttpRequest request = exchange.getRequest().mutate().header("X-user-id", userId).build();
                 ServerWebExchange mutatedExchange = exchange.mutate().request(request).build();
                 return chain.filter(mutatedExchange);
@@ -26,4 +48,47 @@ public class JwtToHeaderGlobalFilter implements GlobalFilter {
             return chain.filter(exchange);
         });
     }
+
+    private User buildUserFromJwt(Jwt jwt) {
+        String userName = jwt.hasClaim("preferred_username") ? jwt.getClaim("preferred_username") : null;
+        String name = jwt.hasClaim("name") ? jwt.getClaim("name") : null;
+        String mobileNumber = jwt.hasClaim("phone_number") ? jwt.getClaim("phone_number") : null;
+        String emailId = jwt.hasClaim("email") ? jwt.getClaim("email") : null;
+        String uuid = jwt.hasClaim("sid") ? jwt.getClaim("sid") : null;
+
+        String type = "EMPLOYEE";
+
+        String issuer = jwt.hasClaim("iss") ? jwt.getClaim("iss") : null;
+        String tenantId = extractRealmFromIssuer(issuer);
+
+        Map<String, Object> realmAccess = jwt.hasClaim("realm_access") ? jwt.getClaimAsMap("realm_access") : null;
+        List<String> rolesString = realmAccess != null && realmAccess.containsKey("roles") ? (List<String>) realmAccess.get("roles"): null;
+        List<Role> roles = rolesString != null ? rolesString.stream().map(role -> Role.builder().name(role).code(role).tenantId(tenantId).build()).toList() : null;
+
+        return User.builder()
+                .userName(userName)
+                .name(name)
+                .type(type)
+                .mobileNumber(mobileNumber)
+                .emailId(emailId)
+                .roles(roles)
+                .tenantId(tenantId)
+                .uuid(uuid)
+                .build();
+    }
+
+    private String extractRealmFromIssuer(String issuer) {
+        if (issuer == null || issuer.isEmpty()) {
+            return null; // Return null if the issuer is null or empty
+        }
+        try {
+            // Split the issuer URL by "/" and return the last segment
+            String[] parts = issuer.split("/");
+            return parts[parts.length - 1];
+        } catch (Exception e) {
+            log.error("Error while extracting realm from issuer: {}", e.getMessage());
+            return null;
+        }
+    }
+
 }
