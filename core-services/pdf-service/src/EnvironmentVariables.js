88d0942e6ab300d8ba1145478992eb9a76ec385c
manastanmay-eGov <manastanmay@egovernments.org>
2025-02-05 10:11:24 +0530

    DB SSL added

--- core-services/pdf-service/src/EnvironmentVariables.js
+++ core-services/pdf-service/src/EnvironmentVariables.js
@@ -23,6 +23,7 @@ const envVariables = {
   DB_HOST: process.env.DB_HOST || "localhost",
   DB_NAME: process.env.DB_NAME || "PdfGen",
   DB_PORT: process.env.DB_PORT || 5432,
+  DB_SSL: process.env.DB_SSL || true,
   EGOV_EXTERNAL_HOST: process.env.EGOV_EXTERNAL_HOST || "https://dev.digit.org/" ,
   IS_ENVIRONMENT_CENTRAL_INSTANCE: process.env.IS_ENVIRONMENT_CENTRAL_INSTANCE || false,
   STATE_SCHEMA_INDEX_POSITION_TENANTID: process.env.STATE_SCHEMA_INDEX_POSITION_TENANTID || 1,
