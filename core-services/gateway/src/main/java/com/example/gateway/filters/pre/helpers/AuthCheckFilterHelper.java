50387200701e3c11aa369577124fe88c185f4e22
Palak Garg <86659286+palak-egov@users.noreply.github.com>
2025-03-24 18:47:50 +0530

    gateway-fix

--- core-services/gateway/src/main/java/com/example/gateway/filters/pre/helpers/AuthCheckFilterHelper.java
+++ core-services/gateway/src/main/java/com/example/gateway/filters/pre/helpers/AuthCheckFilterHelper.java
@@ -1,23 +1,27 @@
 package com.example.gateway.filters.pre.helpers;
 
+import com.example.gateway.utils.CommonUtils;
 import com.example.gateway.utils.UserUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.egov.common.contract.request.RequestInfo;
-import org.egov.common.contract.request.User;
+import org.egov.common.utils.MultiStateInstanceUtil;
 import org.egov.tracer.model.CustomException;
 import org.reactivestreams.Publisher;
+import org.slf4j.MDC;
 import org.springframework.cloud.gateway.filter.factory.rewrite.RewriteFunction;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
-import java.util.HashMap;
 import java.util.Map;
 
 import static com.example.gateway.constants.GatewayConstants.AUTH_TOKEN;
+import java.util.Set;
+
 import static com.example.gateway.constants.GatewayConstants.REQUEST_INFO_FIELD_NAME_PASCAL_CASE;
+import static com.example.gateway.constants.GatewayConstants.TENANTID_MDC;
 
 @Slf4j
 @Component
@@ -27,27 +31,36 @@ public class AuthCheckFilterHelper implements RewriteFunction<Map, Map> {
 
     private UserUtils userUtils;
 
-    public AuthCheckFilterHelper(ObjectMapper objectMapper, UserUtils userUtils) {
+    private CommonUtils commonUtils;
+
+    private MultiStateInstanceUtil centralInstanceUtil;
+
+    public AuthCheckFilterHelper(ObjectMapper objectMapper, UserUtils userUtils, CommonUtils commonUtils, MultiStateInstanceUtil centralInstanceUtil) {
         this.objectMapper = objectMapper;
         this.userUtils = userUtils;
+        this.commonUtils = commonUtils;
+        this.centralInstanceUtil = centralInstanceUtil;
     }
 
     @Override
-    public Publisher<Map> apply(ServerWebExchange exchange, Map body) {
-
-        // TODO: Handle cases when body is null but you want to check for authorization
+    public Publisher<Map> apply(ServerWebExchange serverWebExchange, Map body) {
         try {
-            if (ObjectUtils.isEmpty(body)) {
-                String authToken = exchange.getRequest().getHeaders().get(AUTH_TOKEN).get(0);
-                userUtils.getUser(authToken, exchange);
-                return Mono.just(new HashMap<>());
-            } else {
-                RequestInfo requestInfo = objectMapper.convertValue(body.get(REQUEST_INFO_FIELD_NAME_PASCAL_CASE), RequestInfo.class);
-                requestInfo.setUserInfo(userUtils.getUser(requestInfo.getAuthToken(), exchange));
-                body.put(REQUEST_INFO_FIELD_NAME_PASCAL_CASE, requestInfo);
-                return Mono.just(body);
+            RequestInfo requestInfo = objectMapper.convertValue(body.get(REQUEST_INFO_FIELD_NAME_PASCAL_CASE), RequestInfo.class);
+            requestInfo.setUserInfo(userUtils.getUser(requestInfo.getAuthToken()));
+            body.put(REQUEST_INFO_FIELD_NAME_PASCAL_CASE, requestInfo);
+
+            if (centralInstanceUtil.getIsEnvironmentCentralInstance()) {
+
+                Set<String> tenantIds = commonUtils.validateRequestAndSetRequestTenantId(serverWebExchange, body);
+                /*
+                 * Adding tenantId to header for tracer logging with correlation-id and routing
+                 */
+                String singleTenantId = commonUtils.getLowLevelTenatFromSet(tenantIds);
+                MDC.put(TENANTID_MDC, singleTenantId);
+                serverWebExchange.getAttributes().put(TENANTID_MDC, singleTenantId);
             }
 
+            return Mono.just(body);
         } catch (Exception ex) {
             log.error("An error occurred in Auth check filter", ex);
 
