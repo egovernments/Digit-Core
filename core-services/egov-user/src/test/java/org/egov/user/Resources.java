84b8041ff78f674c91e03381a05171dbbe4a30da
anishEgov <anish.sharma@egovernments.org>
2025-06-06 11:12:18 +0530

    Mock MdmsFetcher for tests

--- core-services/egov-user/src/test/java/org/egov/user/Resources.java
+++ core-services/egov-user/src/test/java/org/egov/user/Resources.java
@@ -1,5 +1,7 @@
 package org.egov.user;
 
+import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
@@ -12,6 +14,30 @@ public class Resources {
                     .replace(" ", "").replace("\n", "");
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    public static JSONArray getFileJSONArrayContents(String fileName) {
+        try {
+            String json = IOUtils.toString(Resources.class.getClassLoader()
+                            .getResourceAsStream(fileName), "UTF-8")
+                    .replace(" ", "").replace("\n", "");
+
+            // Parse JSON array using JsonPath
+            Object result = JsonPath.read(json, "$");
+
+            JSONArray jsonArray = new JSONArray();
+
+            if (result instanceof java.util.List<?>) {
+                jsonArray.addAll((java.util.List<?>) result);
+            } else {
+                throw new RuntimeException("Expected a List at root of JSON but found: " + result.getClass());
+            }
+
+            return jsonArray;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read JSON array from " + fileName, e);
         }
     }
 }
