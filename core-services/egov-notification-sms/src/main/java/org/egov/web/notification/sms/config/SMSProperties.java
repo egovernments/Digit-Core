866f39a811241102978665133aff0579df3fa34b
nitish-egov <137176807+nitish-egov@users.noreply.github.com>
2025-12-09 14:45:21 +0530

    refactor

--- core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/config/SMSProperties.java
+++ core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/config/SMSProperties.java
@@ -69,10 +69,10 @@ public class SMSProperties {
     @Value("${sms.whitelist.numbers}")
     private List<String> whitelistNumbers;
 
-    @Value("${sms.mobile.validation.pattern:^[6-9][0-9]{9}$}")
+    @Value("${sms.mobile.validation.pattern}")
     private String mobileValidationPattern;
 
-    @Value("${sms.mobile.validation.error.message:Mobile number should be 10 digits starting with 6, 7, 8 or 9}")
+    @Value("${sms.mobile.validation.error.message}")
     private String mobileValidationErrorMessage;
 
     @Setter(AccessLevel.PROTECTED) private List<Pattern> whitelistPatterns;
