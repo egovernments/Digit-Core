a81e1a8abc79a754f80f202beaac54585a1c4386
vinothrallapalli-egov <vinoth.r@egovernments.org>
2026-02-17 12:42:26 +0530

    Fixed the compilation error and missing dependency in service request

--- core-services/mdms-v2/src/main/java/org/egov/infra/mdms/constants/SchemaDefinitionConstant.java
+++ core-services/mdms-v2/src/main/java/org/egov/infra/mdms/constants/SchemaDefinitionConstant.java
@@ -1,4 +1,7 @@
 package org.egov.infra.mdms.constants;
 
 public class SchemaDefinitionConstant {
+    public static final String RES_MSG_ID = "Successful";
+    public static final String SUCCESSFUL = "SUCCESSFUL";
+    public static final String FAILED = "FAILED";
 }
