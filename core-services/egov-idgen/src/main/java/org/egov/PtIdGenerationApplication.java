2f5744a2dd4b4c9fa523329d0571b37ec17216a2
hruthvikl-egov <hruthvik.lingampet@egovernments.org>
2026-07-08 12:41:11 +0530

    cache MDMS id-format/city lookups to survive bulk generation in idgen service

--- core-services/egov-idgen/src/main/java/org/egov/PtIdGenerationApplication.java
+++ core-services/egov-idgen/src/main/java/org/egov/PtIdGenerationApplication.java
@@ -3,15 +3,17 @@ package org.egov;
 import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Import;
 
 /**
  * Description : This is initialization class for pt-idGeneration module
- * 
+ *
  * @author Pavan Kumar Kamma
  *
  */
 @SpringBootApplication
+@EnableCaching
 @Import({TracerConfiguration.class})
 public class PtIdGenerationApplication {
 	public static void main(String[] args) {
